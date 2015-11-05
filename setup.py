cbffaa6d9dac27027440da087008c92595b24a16
Diogo Alves <alves_diogo@hotmail.com>
2015-11-05 12:54:07 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name = "pythonCRC",
+    packages = ["pythonCRC"],
+    version = "1.0.0",
+    description = "Easy to yse CRC calculator",
+    author = "Diogo Alves",
+    author_email = "diogo.alves@mafralab.com",
+    url = "https://github.com/killercode/PythonCRC/",
+    download_url = "http://chardet.feedparser.org/download/python3-chardet-1.0.1.tgz",
+    keywords = ["CRC"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ]
+)
